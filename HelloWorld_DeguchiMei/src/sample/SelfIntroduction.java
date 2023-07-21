e89513a504a4d7e69d9d96fe9b183b047e878638
meeeeei <meeeeei@jeminyouminujin.elecom>
2023-07-21 19:49:38 +0900

    更新

---
+++ HelloWorld_DeguchiMei/src/sample/SelfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//氏名
+		String name = "出口　芽依";
+		//年齢
+		int age = 25;
+		//趣味
+		String hobby = " アニメ鑑賞";
+				
+		/*
+		 * 自己紹介分を出力する処理
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
