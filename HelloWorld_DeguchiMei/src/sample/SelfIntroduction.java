de739861209c5fba7b2ea9ef5844d30108f7d73a
meeeeei <meeeeei@jeminyouminujin.elecom>
2023-08-03 16:00:29 +0900

    // の後にスペースを追加

--- HelloWorld_DeguchiMei/src/sample/SelfIntroduction.java
+++ HelloWorld_DeguchiMei/src/sample/SelfIntroduction.java
@@ -4,15 +4,15 @@ public class SelfIntroduction {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		//氏名
+		// 氏名
 		String name = "出口　芽依";
-		//年齢
+		// 年齢
 		int age = 25;
-		//趣味
+		// 趣味
 		String hobby = "アニメ鑑賞";
 				
 		/*
-		 * 自己紹介分を出力する処理
+		 *  自己紹介分を出力する処理
 		 */
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "歳です。");
