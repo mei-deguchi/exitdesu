8f51f7b8c8deb0043d9182fa4e687657ceb97af3
meeeeei <meeeeei@localhost>
2023-11-20 23:18:32 +0900

    カリキュラムb-4.5 提出

--- kadai/src/curriculum_B/Qes4.java
+++ kadai/src/curriculum_B/Qes4.java
@@ -7,25 +7,19 @@ public class Qes4 {
 		// for文を使って１から９まで繰り返す
 		for (int i = 1; i < 10; i++) {
 			for (int j = 1; j < 10; j++) {
-				 
-				
+
+				// formatメソッドを使って九九の式を出力
+				String kuku =(String.format("%02d * %02d = %02d ", i, j, i*j));	
+				System.out.printf(kuku);
+
 				// 九九の式の間に｜｜を表示
-				if (10 > i && 10 > j) {
+				if(i != 0 && j != 9){
 					System.out.print("|| ");
-				
-					// formatメソッドを使って九九の式を出力
-					String kuku =(String.format("%02d * %02d = %02d ", i, j, i*j));	
-					System.out.printf(kuku);
-			
 				}
-		
 			}
-			
-			// 行を改行
+
+			// １周回った後改行
 			System.out.println();
 		}
-		
-
 	}
-
 }
--- kadai/src/curriculum_B/Qes5.java
+++ kadai/src/curriculum_B/Qes5.java
@@ -1,25 +1,26 @@
 package curriculum_B;
 
 public class Qes5 {
-	
-	public static void main(String[] args) {
-		
-		int [] [] kuku = new int [21][10];
-		//
-		for(int i=1; i<21; i++) {
-			for(int j=1; j<10; j++) {
-				kuku [i][j] = (i+1) * (j+1);
-			
-				
-			
-			System.out.print(kuku[i][j]);
-			System.out.print(" ");
-				
-			}
-			
-			System.out.println("");
-		}
-		
-	}
 
+	public static void main(String[] args) {
+
+		// for文を使ってjは9まで、iは20まで繰り返す
+		for (int j = 1; j < 10; j++) {
+			for (int i = 1; i < 21; i++) {
+
+				// formatメソッドを使って九九の式を出力
+				String kuku = (String.format("%03d * %03d = %03d ", i, j, i * j));
+				System.out.printf(kuku);
+
+				// 九九の式の間に｜｜を表示
+				if (i != 20 && j != 10) {
+					System.out.print("|| ");
+
+				}
+			}
+			// 1周回った後改行
+			System.out.println();
+
+		}
+	}
 }
