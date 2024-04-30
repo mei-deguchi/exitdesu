6ff5786e8aeae6ca13638c74f6447fed2194ee97
meeeeei <meeeeei@localhost>
2024-04-30 18:28:03 +0900

    確認用

--- kadai/src/curriculum_19/Animal.java
+++ kadai/src/curriculum_19/Animal.java
@@ -7,11 +7,13 @@ public class Animal {
 	public static void main(String[] args) {
 		Dog.count();
 		Dog animal = new Dog("キリン");
-		//dog.animalName = "キリン";
+		
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		System.out.println(animal.animalName);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		System.out.println(animal.count);
+		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		LocalDateTime ldt = LocalDateTime.now();
 		System.out.println(ldt);
--- kadai/src/curriculum_19/Dog.java
+++ kadai/src/curriculum_19/Dog.java
@@ -3,6 +3,7 @@ package curriculum_19;
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	String animalName;
+	
 	// Q2：フィールドに動物の数の変数を定義してください。
 	int count;
 	
