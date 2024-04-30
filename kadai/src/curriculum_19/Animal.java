9b3cf24bd07a2b8880aae3287d178a646d211726
meeeeei <meeeeei@localhost>
2024-04-30 18:24:20 +0900

    提出用

---
+++ kadai/src/curriculum_19/Animal.java
@@ -0,0 +1,19 @@
+package curriculum_19;
+
+import java.time.LocalDateTime;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		//Dog.printCount();
+		Dog animal = new Dog("キリン");
+		//dog.animalName = "キリン";
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		System.out.println(animal.animalName);
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		System.out.println(animal.count);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		LocalDateTime ldt = LocalDateTime.now();
+		System.out.println(ldt);
+	}
+} 
---
+++ kadai/src/curriculum_19/Dog.java
@@ -0,0 +1,18 @@
+package curriculum_19;
+
+public class Dog {
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String animalName;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int count;
+	
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	Dog(String animalName) {
+		Dog.count ++;
+		this.animalName = animalName;
+	}
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	Dog(int num){
+		this.count = num;
+	}
+}
