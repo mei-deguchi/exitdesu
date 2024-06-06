96079ac40f471b289a39b9d7434e477a7d1be8d5
meeeeei <meeeeei@jeminyouminujin.elecom>
2024-06-07 01:10:32 +0900

    6/7 再提出分

--- kadai/src/curriculum_19/Animal.java
+++ kadai/src/curriculum_19/Animal.java
@@ -1,26 +1,28 @@
 package curriculum_19;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Animal {
 	
 	public static void main(String[] args) {
-		Dog kirin = new Dog("キリン", 30);		// キリンインスタンス作成
-		Dog elephant = new Dog("ゾウ", 10);		// ゾウインスタンス作成
-
-		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog dog = new Dog();		// インスタンス作成
+		dog.name = "ダックスフンド";
+		dog.count = 2;
+		// Q1：Dogクラスを呼び出して変数に代入し、犬の名前をコンソールに出力してください。
 		// コンソールに動物名を出力
-		System.out.println(kirin.animalName);
-		System.out.println(elephant.animalName);
+		System.out.println(dog.name);
 		
-		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		// Q2：Dogクラスを呼び出して変数に代入し、犬の数をコンソールに出力してください。
 		// 動物の数を出力
-		System.out.println(kirin.count);
-		System.out.println(elephant.count);
+		System.out.println(dog.count);
 
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		// 日付出力
 		LocalDateTime ldt = LocalDateTime.now();
-		System.out.println(ldt);
+		
+		// フォーマット指定
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println(ldt.format(f));
 	}
 }
--- kadai/src/curriculum_19/Dog.java
+++ kadai/src/curriculum_19/Dog.java
@@ -1,21 +1,24 @@
 package curriculum_19;
 
 public class Dog {
-	// Q1：フィールドに動物の名前の変数を定義してください。
-	String animalName;	// 動物名
+	private static final String DogName = null;
+
+	// Q1：フィールドに犬の名前の変数を定義してください。
+	String name;	// 動物名
 	
-	// Q2：フィールドに動物の数の変数を定義してください。
+	// Q2：フィールドに犬の数の変数を定義してください。
 	int count;			// 動物の数
 	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	// 動物名と動物の数の初期値を代入
-	Dog(String animalName, int number) {
+	// 犬の名前と犬の数の初期値を代入
+	public Dog() {
+		// TODO 自動生成されたコンストラクター・スタブ
 		count ++;
-		this.animalName = animalName;
-		Count(number);
+		this.name = DogName;
 	}
+
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	// 動物の数の代入　問題文が分かれているため、別途作成
+	// 犬の数の代入　問題文が分かれているため、別途作成
 	public void Count(int number){
 		this.count = number;
 	}
