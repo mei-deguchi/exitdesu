5cd60943c5a82bdb2931b7fef4dd5af1c597e80b
meeeeei <meeeeei@jeminyouminujin.elecom>
2024-07-21 16:15:54 +0900

    カリキュラム２０提出分

---
+++ kadai/src/curriculum_20/Hello.java
@@ -0,0 +1,16 @@
+package curriculum_20;
+
+public class Hello {
+	// フィールド変数
+	String country;
+	String food;
+	String countryFood;
+	int today;
+	
+	// コンストラクタ実行
+	public Hello(String country, String food, String countryFood) {
+		this.country = country;
+		this.food = food;
+		this.countryFood = countryFood;	
+	}
+}
---
+++ kadai/src/curriculum_20/Main.java
@@ -0,0 +1,26 @@
+package curriculum_20;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// インスタンス作成
+		Hello hello= new Hello("日本", "寿司", "和食");
+		
+		// コンソール出力
+		System.out.println("こんにちは！ここは" + hello.country + "です！");
+		System.out.println("この" + hello.food + "はうまい");
+		System.out.println(hello.food + "は" + hello.countryFood + "です");
+		
+		// 日時出力
+		LocalDateTime ldt = LocalDateTime.now();
+		
+		// フォーマット指定
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		System.out.println("今の現在日時は" + ldt.format(f) + "です");
+		
+	}
+
+}
