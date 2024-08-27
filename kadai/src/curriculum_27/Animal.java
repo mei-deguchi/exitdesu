d95425e0484911a1ffeb2984e8e1349f9274bcbb
meeeeei <meeeeei@fe80:0:0:0:aede:48ff:fe00:1122%4>
2024-08-27 19:39:02 +0900

    8/27提出分

---
+++ kadai/src/curriculum_27/Animal.java
@@ -0,0 +1,76 @@
+package curriculum_27;
+
+public class Animal {
+
+	// フィールド変数
+	private String name;
+	private double size;
+	private int speed;
+	private String scientificName;
+
+	// コンストラクタ実行
+	Animal(String name, double size, int speed){
+		this.name = name;
+		this.size = size;
+		this.speed = speed;
+		this.scientificName = scientificName(name);
+	}
+	
+	// 学名を取得するメソッド
+	private String scientificName(String name) {
+		
+		// 動物の名前によって学名を取得
+		switch (name) {
+		
+		case "ライオン":
+			return "パンテラ レオ";
+			
+		case "ゾウ":
+			return "ロキソドンタ・サイクロティス";
+			
+		case "パンダ":
+			return "アイルロポダ・メロノレウカ";
+			
+		case "チンパンジー":
+			return "パン・トゥログロディテス";
+			
+		case "シマウマ":
+			return "チャップマンシマウマ";
+			
+		case "インコ":
+			return "不明";
+		}
+
+		return name;
+	}
+
+	// getterとsetter
+	public String getName() {
+		return this.name;
+	}
+
+	public void setAnimal(String name) {
+		this.name = name;
+	}
+
+	public double getSize() {
+		return this.size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+
+	public void setSpped(int speed) {
+		this.speed = speed;
+	}
+
+	public String getScientificName() {
+		return this.scientificName;
+	}
+
+}
---
+++ kadai/src/curriculum_27/Main.java
@@ -0,0 +1,47 @@
+package curriculum_27;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// 文字列の受け取り
+		Scanner sc = new Scanner(System.in);
+		System.out.println("コンソールに文字を入力してください");
+		String input = sc.nextLine();
+
+		// 複数の動物の情報を区切るためのコンマ
+		String[] animal = input.split(",");
+
+		// 各動物の情報を処理する
+		for (String animal1 : animal) {
+			
+			// :で情報を区切る
+			String[] parts = animal1.split(":");
+			
+			// 動物名：体長：速度を入力した時の情報を取得
+			if(parts.length == 3) {
+
+				// 動物名：体長：速度の順番を指定
+				String name = parts[0];
+				double size = Double.parseDouble(parts[1]);
+				int speed = Integer.parseInt(parts[2]);
+
+				// インスタンス作成
+				Animal animals = new Animal(name, size, speed);
+
+				//コンソール出力
+				System.out.println("動物名:" + animals.getName());
+				System.out.println("体長:" + animals.getSize() + "m");
+				System.out.println("速度:" + animals.getSpeed() + "km/h");
+				System.out.println("学名:" + animals.getScientificName());
+				System.out.println();
+			}
+
+		}
+		
+		// スキャナ閉じる
+		sc.close();
+	}
+}
